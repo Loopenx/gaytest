49b099896534fdea333a4e439ea5de2defcff0d8
Loopenx <124714389+Loopenx@users.noreply.github.com>
2023-10-01 15:20:02 +0300

    Add files via upload

---
+++ gaytesti.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+def no():
+    messagebox.showinfo(' ','anketi tamamladığın için eyv')
+    quit()
+
+def motionMouse (event):
+    btnYes.place(x=random.randint(0, 500), y=random.randint(0, 500))
+
+root = Tk()
+root.geometry('600x600')
+root.title('Anket')
+root.resizable (width=False, height=False)
+root['bg'] = 'white'
+label = Label(root, text='Gaymisin?', font='Arial 20 bold', bg='white').pack()
+btnYes = Button (root, text='Hayır', font='Arial 20 bold')
+btnYes.place(x=170, y=100)
+btnYes.bind('<Enter>', motionMouse)
+btnNo = Button(root, text='Evet', font='Arial 20 bold', command=no) .place (x=350, y=100)
+
+root.mainloop()
